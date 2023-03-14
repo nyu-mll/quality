57224dd6853960f6072d276b0066d08c73ce3f8e
yzpang <yzpang@nyu.edu>
2023-03-14 02:21:14 -0400

    new lrqa

--- baselines/lrqa/utils/model_tweaks.py
+++
@@ -1,7 +0,0 @@
-import transformers
-
-
-def adjust_tokenizer(tokenizer):
-    if isinstance(tokenizer, (transformers.GPT2Tokenizer, transformers.GPT2TokenizerFast)) and \
-            "gpt" in tokenizer.name_or_path:
-        tokenizer.pad_token = tokenizer.eos_token
